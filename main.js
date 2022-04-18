0ef0a347b33546870a42b4a9d964a2b075d6161a
s20020 <s20020@std.it-college.ac.jp>
2022-04-18 11:22:19 +0900

    ボール方向転換完了。

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-//canvasへの参照を取得する
 var canvas = document.getElementById("myCanvas");
-//実際にCanvasに描写するために２Dを使う
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
